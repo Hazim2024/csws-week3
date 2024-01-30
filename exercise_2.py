1bab75797035912a7458cb5374d6d0c13dd08c3c
Hazim <hazimiftikhar@gmail.com>
2024-01-30 11:41:50 +0000

    Added exercise.py

---
+++ exercise_2.py
@@ -0,0 +1,20 @@
+#4-4
+##print(list(range(1,1000000)))##
+
+#4-5
+List=list(range(1,1000000))
+maxList=max(List)
+print(maxList)
+minList=min(List)
+print(minList)
+sumList=sum(List)
+print(sumList)
+
+#4-6
+for i in range(0,20):
+    oddNum=i%2
+    if oddNum!=0:
+        print(i)
+
+#4-7
+
